2b90c8200b125c5c2314a5759de24b8bea97d60c
Andresrom1123 <amclres@gmail.com>
2020-07-21 17:24:25 -0500

    email unique

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
@@ -8,7 +9,7 @@ from topics.models import Topic
 class Question(models.Model):
     question = models.CharField(max_length=200)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='topic_question')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_question')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_question')
     followers = models.ManyToManyField(User, related_name='followers_question')
     created_at = models.DateTimeField(auto_created=timezone.now)
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from rest_framework.exceptions import ValidationError
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
