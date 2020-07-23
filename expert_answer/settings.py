8a24f3ff9cac75a4b9e355d5065737cd1f49a059
Andresrom1123 <amclres@gmail.com>
2020-07-23 13:34:29 -0500

    Updated Settings

--- expert_answer/settings.py
+++ expert_answer/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=3%z=k@6fd_x^_y0!hza$h8%y8p)d%(ztr=l@muvjm9ob=3opf'
+# SECRET_KEY = '=3%z=k@6fd_x^_y0!hza$h8%y8p)d%(ztr=l@muvjm9ob=3opf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['expertanswerapp.herokuapp.com']
 
 # Application definition
 
